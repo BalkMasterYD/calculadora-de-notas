657a8b7995190b0132eab2cdb58b8fcfb99b9bf7
yago dias <100420066+BalkMasterYD@users.noreply.github.com>
2024-10-11 18:18:58 -0300

    final project

--- unit1.pas
+++ unit1.pas
@@ -46,32 +46,28 @@ procedure TForm1.Button1Click(Sender: TObject);
 var
   Num1, Num2, Num3, Num4, Media: Double;
 begin
-  // Verifica se os campos estão preenchidos
   if (Edit1.Text = '') or (Edit2.Text = '') or (Edit3.Text = '') or (Edit4.Text = '') then
   begin
     ShowMessage('Por favor, preencha todos os campos.');
     Exit;
   end;
 
-  // Converte os textos para números
   Num1 := StrToFloat(Edit1.Text);
   Num2 := StrToFloat(Edit2.Text);
   Num3 := StrToFloat(Edit3.Text);
   Num4 := StrToFloat(Edit4.Text);
 
-  // Calcula a média
   Media := (Num1 + Num2 + Num3 + Num4) / 4;
 
-  // Verifica a média e envia para o formulário correspondente
   if Media < 5.9 then
   begin
     Form2.Show;
-    Form1.Hide; // Supondo que Form2 é o formulário atual
+    Form1.Hide; 
   end
   else
   begin
     Form3.Show;
-    Form1.Hide; // Supondo que Form2 é o formulário atual
+    Form1.Hide; 
   end;
 end;
 
@@ -81,4 +77,3 @@ begin
 end;
 
 end.
-
